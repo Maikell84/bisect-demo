daf804357fc056b769d488a9835ff9de0505dae6
Michael Blum <michael.blum@mobile-function.com>
2018-08-30 21:28:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var bug = false;
+
+if(bug){
+    document.querySelector('#smile').style.display = 'none';
+    document.querySelector('#bug').style.display = 'block';
+} else {
+    document.querySelector('#smile').style.display = 'block';
+    document.querySelector('#bug').style.display = 'none';
+}
