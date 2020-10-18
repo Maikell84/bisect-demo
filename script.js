427684a6a657c7fda4625c1dcbe988176038b28a
Michael Blum <blummichael@gmail.com>
2020-10-18 22:02:30 +0200

    Autoformat code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var bug = true;
 
-if(bug){
-    document.querySelector('#smile').style.display = 'none';
-    document.querySelector('#bug').style.display = 'block';
+if (bug) {
+  document.querySelector("#smile").style.display = "none";
+  document.querySelector("#bug").style.display = "block";
 } else {
-    document.querySelector('#smile').style.display = 'block';
-    document.querySelector('#bug').style.display = 'none';
+  document.querySelector("#smile").style.display = "block";
+  document.querySelector("#bug").style.display = "none";
 }
